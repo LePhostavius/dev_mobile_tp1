789c5d37dab186328e2058f36f1cf4ca44951ccc
Louis Falce <louis.falce@hotmail.fr>
2022-05-13 18:31:57 +0200

    restart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -6,7 +6,14 @@ void main() {
   runApp(ConvertisseurDeMesures());
 }
 
-class ConvertisseurDeMesures extends StatelessWidget {
+
+
+class ConvertisseurDeMesures extends StatefulWidget {
+  @override
+  State<ConvertisseurDeMesures> createState() => _ConvertisseurDeMesuresState();
+}
+
+class _ConvertisseurDeMesuresState extends State<ConvertisseurDeMesures> {
 @override
 Widget build(BuildContext context) {
 const appTitle = 'Convertisseur de mesures';
@@ -16,126 +23,27 @@ home: Scaffold(
 appBar: AppBar(
 title: Text(appTitle),
 ),
-body : Center(child: MyCustomForm()
+body : Center(child: MesWidgets()
 ),
 ));
 }
 }
 
 
-class TextTest extends StatelessWidget {
+class MesWidgets extends StatefulWidget {
 @override
-Widget build(BuildContext context) {
-return Scaffold(
- body : Text('bonjour'),
-);
+MesWidgetsState createState() {
+return MesWidgetsState();
 }
 }
 
-
-
-
-
-
-
-
-class MyCustomForm extends StatefulWidget {
-@override
-MyCustomFormState createState() {
-return MyCustomFormState();
-}
-}
-class MyCustomFormState extends State<MyCustomForm> {
-  late String _dropdownvalue;
-
-  String? _uniteDepart;
-  String? _uniteConvertion;
-
-  late String _nombreSaisi;
-
-  final List<String> _unitesMesures = [
-    'mètres',
-    'kilomètres',
-    'grammes',
-    'kilogrammes',
-    'pieds',
-    'miles',
-    'livres',
-    'onces'
-  ];
-
-DropdownMenuItem<String> buildMenuItem(String item)=>  DropdownMenuItem(
-  value: item,
-  child:Text(item,style:TextStyle(fontWeight: FontWeight.bold,fontSize:20),)
-);
-
+class MesWidgetsState extends State<MesWidgets>
+{
   @override
-  void initState() {
-     _nombreSaisi = "ici apparaitra votre valeur à convertir";
-     _dropdownvalue = "plus tard";
-    // TODO: implement initState
-    super.initState();
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
-
-  void Changement(String a)
-  {
-    setState(() {
-      _nombreSaisi = '$a';
-      print(_nombreSaisi);
-    });
-  }
-
-@override
-Widget build(BuildContext context) {
-return Column(
-crossAxisAlignment: CrossAxisAlignment.start,
-children: <Widget>[
-Padding(
-padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-child: TextField(
-keyboardType: TextInputType.number,
-decoration: InputDecoration(
-border: OutlineInputBorder(),
-hintText: 'Saisir le mot recherché', 
-), onChanged: Changement
-),
-),
-Padding(
-padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-child: TextField(
-decoration: InputDecoration(
-border: UnderlineInputBorder(),
-labelText: '$_nombreSaisi',
-),
-),
-),
-Padding(
-padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-child: Text('Depuis'),
-),
-Padding(
-padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-child : DropdownButton<String>(value: _uniteDepart,items: _unitesMesures.map(buildMenuItem).toList(),
-onChanged: (value) => setState((() => this._uniteDepart = value),
-),
-)
-),
-Padding(
-padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-child: Text('Vers'),
-),
-Padding(
-padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-child : DropdownButton<String>(value: _uniteConvertion,items: _unitesMesures.map(buildMenuItem).toList(),
-onChanged: (value) => setState((() => this._uniteConvertion = value),
-),
-)
-),
-Padding(
-  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-  child : TextTest(),
-)]
-);
-}
+  
 }
 
