a630db564a5775ba237745e6d96eb991b8f23c2f
Louis Falce <louis.falce@hotmail.fr>
2022-05-17 22:35:39 +0200

    debut de la reelle app

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -6,18 +6,69 @@ void main() {
   runApp(ConvertisseurDeMesures());
 }
 
-class ConvertisseurDeMesures extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-const appTitle = 'Convertisseur de mesures';
-return MaterialApp(
-title: appTitle,
-home: Scaffold(
-appBar: AppBar(
-title: Text(appTitle),
-),
-),
-);
-}
+class ConvertisseurDeMesures extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Convertisseur de',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Convertisseur de mesures')),
+        body: Center(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.all(10.0)),
+              v_a_c(),
+              Padding(padding: EdgeInsets.all(10.0)),
+              depuis(),
+              Padding(padding: EdgeInsets.all(10.0)),
+              vers()
+            ],
+          ),)
+        )
+    );
+  }
 }
 
+
+
+class v_a_c extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'valeur à convertir',
+      style: TextStyle(
+        color: Colors.blue,
+        fontSize: 17,
+        fontWeight: FontWeight.w500
+       ),
+    );
+  }
+} 
+
+class depuis extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'depuis',
+      style: TextStyle(
+        color: Colors.orange,
+        fontSize: 15,
+        fontWeight: FontWeight.w500
+       ),
+    );
+  }
+}
+
+class vers extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'vers',
+      style: TextStyle(
+        color: Colors.orange,
+        fontSize: 15,
+        fontWeight: FontWeight.w500
+       ),
+    );
+  }
+}
