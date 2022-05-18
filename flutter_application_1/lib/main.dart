6ba2c4386ff51a12b4d024e94dbab54c20565833
Louis Falce <louis.falce@hotmail.fr>
2022-05-18 16:28:38 +0200

    final

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,184 +1,137 @@
-
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(ConvertisseurDeMesures());
+  runApp(MyApp());
 }
 
-class ConvertisseurDeMesures extends StatefulWidget{
+class MyApp extends StatefulWidget {
   @override
-  State<ConvertisseurDeMesures> createState() => _ConvertisseurDeMesuresState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-void initState()
-{
-  double _nombreSaisi = 0.0;
-}
+class _MyAppState extends State<MyApp> {
+  double? _nombreSaisi;
+  String? _uniteConvertion;
+  String? _message;
+  String? _uniteDepart;
 
-class _ConvertisseurDeMesuresState extends State<ConvertisseurDeMesures> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Convertisseur de',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Convertisseur de mesures')),
-        body: Container(
-          padding : EdgeInsets.symmetric(horizontal: 20),
-          child : Column(
-            children: [v_a_c(),Spacer(),DataToConvert(),Spacer(),depuis(),unite_depart(),Spacer(),vers(),unite_de_convertion()],
-          )
-        )
-      ));}
-}
-
-
-class DataToConvert extends StatefulWidget {
-  @override
-  State<DataToConvert> createState() => _DataToConvertState();
-}
-
-class _DataToConvertState extends State<DataToConvert> {
-  @override
-  Widget build(BuildContext context){
-    return TextFormField(
-      keyboardType: TextInputType.number,
-      decoration: const InputDecoration(
-        border: UnderlineInputBorder(),
-        labelText: 'Entrer la valeur à convertir',
-      ),
-    );
-  }
-}
-
-
-class unite_depart extends StatefulWidget {
-
-
-  @override
-  State<unite_depart> createState() => _unite_departState();
-}
-
-class unite_de_convertion extends StatefulWidget {
-
-
-  @override
-  State<unite_de_convertion> createState() => _unite_de_convertionState();
-}
-
-class _unite_de_convertionState extends State<unite_de_convertion> {
-
-String _uniteDeConvertion = "";
- 
- final List<String> _unitesMesures = [
-    'mètres',
-    'kilomètres',
-    'grammes',
-    'kilogrammes',
-    'pieds',
-    'miles',
-    'livres',
-    'onces'
-  ];
-
-  @override
-  void initState()
-  {
-    _uniteDeConvertion = _unitesMesures.first;
-  }
-
-  DropdownMenuItem<String> buildMenuItem(String item)=>  DropdownMenuItem(
-  value: item,
-  child:Text(item,style:TextStyle(fontWeight: FontWeight.bold,fontSize:20),)
-);
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(value: _uniteDeConvertion,items: _unitesMesures.map(buildMenuItem).toList(),
-     onChanged: (value) => setState(((() => this._uniteDeConvertion = value!))));
-     
-  }
-}
-
-
-
-class _unite_departState extends State<unite_depart> {
-
-String _uniteDepart = "";
- 
- final List<String> _unitesMesures = [
-    'mètres',
-    'kilomètres',
-    'grammes',
-    'kilogrammes',
-    'pieds',
-    'miles',
-    'livres',
-    'onces'
-  ];
-
-  @override
-  void initState()
-  {
-    _uniteDepart = _unitesMesures.first;
-  }
-
-  DropdownMenuItem<String> buildMenuItem(String item)=>  DropdownMenuItem(
-  value: item,
-  child:Text(item,style:TextStyle(fontWeight: FontWeight.bold,fontSize:20),)
-);
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(value: _uniteDepart,items: _unitesMesures.map(buildMenuItem).toList(),
-     onChanged: (value) => setState(((() => this._uniteDepart = value!))));
-     
-  }
-}
-
-
-
-class v_a_c extends StatelessWidget {
-  @override
-   
-  Widget build(BuildContext context) {
-    final TextStyle styleEntree = TextStyle(
+  final TextStyle styleEntree = TextStyle(
     fontSize: 20,
     color: Colors.blue[900],
-    );
-    return Text(
-      'Valeur à convertir',
-       style: styleEntree
-    );
-  }
-}
+  );
+  final TextStyle styleLabel = TextStyle(
+    fontSize: 20,
+    color: Colors.grey[700],
+  );
 
-class depuis extends StatelessWidget {
+  @override
+  void initState() {
+    super.initState();
+    _nombreSaisi = 0.0;
+    _uniteConvertion = 'mètres';
+    _uniteDepart = 'mètres';
+  }
+
+  final Map<String, int> _mesuresMap = {
+    'mètres': 0,
+    'kilomètres': 1,
+    'grammes': 2,
+    'kilogrammes': 3,
+    'pieds': 4,
+    'miles': 5,
+    'livres': 6,
+    'onces': 7
+  };
+
+  final dynamic _formules = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 0, 0, 0.0625, 1],
+  };
+
+  void convertir(double valeur, String depuis, String vers) {
+    int numDepuis = _mesuresMap[depuis] ?? 0;
+    int numVers = _mesuresMap[vers] ?? 0;
+    var multiplicateur = _formules[numDepuis.toString()][numVers];
+    var resultat = valeur * multiplicateur;
+    String message;
+    if (resultat == 0) {
+      message = 'Cette conversion ne peut être réalisée!!!';
+    } else {
+      message =
+          '${valeur.toString()} $depuis\n est égal à\n${resultat.toString()} $vers';
+    }
+    setState(() {
+      _message = message;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final TextStyle styleLabel = TextStyle(
- fontSize: 20,
- color: Colors.grey[700],
- );
-    return Text(
-      'depuis',
-      style: styleLabel
-    );
+    DropdownMenuItem<String> buildMenuItem(String item) =>
+        DropdownMenuItem(value: item, child: Text(item, style: styleLabel));
+
+    return MaterialApp(
+        title: 'Entête',
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Convertisseur de Mesures')),
+            body: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Text('Valeur à convertir', style: styleEntree),
+                  TextField(
+                    style: styleEntree,
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                        hintText: 'Saisissez la mesure à convertir'),
+                    onChanged: (texte) {
+                      var vr = double.tryParse(texte);
+                      if (vr != null) {
+                        setState(() {
+                          _nombreSaisi = vr;
+                        });
+                      }
+                    },
+                  ),
+                  Text('Depuis', style: styleLabel),
+                  DropdownButton<String>(
+                      value: _uniteDepart,
+                      items: _mesuresMap.keys.map(buildMenuItem).toList(),
+                      onChanged: (newValue) =>
+                          setState((() => _uniteDepart = newValue!))),
+                  Text('Vers', style: styleLabel),
+                  DropdownButton<String>(
+                      value: _uniteConvertion,
+                      items: _mesuresMap.keys.map(buildMenuItem).toList(),
+                      onChanged: (nouvelleUnite) =>
+                          setState((() => _uniteConvertion = nouvelleUnite!))),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (_nombreSaisi == 0) {
+                          return;
+                        } else {
+                          convertir(
+                              _nombreSaisi!, _uniteDepart!, _uniteConvertion!);
+                        }
+                      },
+                      child: Text(
+                        'convertir',
+                        style: styleEntree,
+                      )),
+                  Text(
+                    _message ?? '',
+                    textAlign: TextAlign.center,
+                    style: styleLabel,
+                  )
+                ],
+              ),
+            )));
   }
 }
-
-class vers extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final TextStyle styleLabel = TextStyle(
- fontSize: 20,
- color: Colors.grey[700],
- );
-    return Text(
-      'vers',
-      style: styleLabel
-    );
-  }
-}
-
-
